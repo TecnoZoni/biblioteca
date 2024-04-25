677f59d8c835eebebd019feb170c0b106f4e27c0
Agustin <Admin@DESKTOP-LO6U7DV>
2024-04-25 19:22:49 -0300

    Cambios en el Dashboard

--- src/main/java/com/mycompany/biblioteca/igu/DashBoard.java
+++ src/main/java/com/mycompany/biblioteca/igu/DashBoard.java
@@ -20,23 +20,39 @@ public class DashBoard extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pFondo = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
+
+        pFondo.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout pFondoLayout = new javax.swing.GroupLayout(pFondo);
+        pFondo.setLayout(pFondoLayout);
+        pFondoLayout.setHorizontalGroup(
+            pFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 701, Short.MAX_VALUE)
+        );
+        pFondoLayout.setVerticalGroup(
+            pFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 513, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 701, Short.MAX_VALUE)
+            .addComponent(pFondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 513, Short.MAX_VALUE)
+            .addComponent(pFondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel pFondo;
     // End of variables declaration//GEN-END:variables
 }
