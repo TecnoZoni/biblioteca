14b6f095af1e9fd537e18d939d50c825471850ae
Agustin <>
2024-04-25 19:32:36 -0300

    Cambios en el Dashboard

--- src/main/java/com/mycompany/biblioteca/igu/DashBoard.java
+++ src/main/java/com/mycompany/biblioteca/igu/DashBoard.java
@@ -2,6 +2,7 @@ package com.mycompany.biblioteca.igu;
 
 import com.mycompany.biblioteca.logica.Controladora;
 import com.mycompany.biblioteca.logica.Usuario;
+import java.awt.Color;
 
 public class DashBoard extends javax.swing.JFrame {
 
@@ -21,21 +22,31 @@ public class DashBoard extends javax.swing.JFrame {
     private void initComponents() {
 
         pFondo = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
 
         pFondo.setBackground(new java.awt.Color(255, 255, 255));
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("X");
+
         javax.swing.GroupLayout pFondoLayout = new javax.swing.GroupLayout(pFondo);
         pFondo.setLayout(pFondoLayout);
         pFondoLayout.setHorizontalGroup(
             pFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 701, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pFondoLayout.createSequentialGroup()
+                .addContainerGap(679, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addContainerGap())
         );
         pFondoLayout.setVerticalGroup(
             pFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 513, Short.MAX_VALUE)
+            .addGroup(pFondoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addContainerGap(480, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -53,6 +64,9 @@ public class DashBoard extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel btnCerrar;
+    private javax.swing.JLabel btnCerrar1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel pFondo;
     // End of variables declaration//GEN-END:variables
 }
